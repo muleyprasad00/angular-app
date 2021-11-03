ae607986fe2235d0ec0712706274929e754c6075
unknown <muleyprasad00@gmail.com>
2021-11-04 00:52:59 +0530

    master service added

---
+++ src/app/master.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Apollo, gql, QueryRef } from 'apollo-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MasterService {
+  private query: QueryRef<any> | undefined;
+  constructor(private apollo: Apollo) { }
+
+  get(queryConfig: any, columns: any) {
+    const query = gql`
+      query  ${queryConfig.queryName}($queryInput:QueryInput){
+        ${queryConfig.queryName}(queryInput:$queryInput){
+          data
+        }
+    }
+    `;
+    const reqConfig = queryConfig.variables;
+    reqConfig.columns = columns;
+
+    const variables: any = {
+      queryInput: { ...reqConfig }
+    }
+    this.query = this.apollo.watchQuery({
+      query,
+      variables
+    });
+
+   return this.query.valueChanges
+  }
+
+  getById(){
+
+  }
+
+  create(){
+
+  }
+
+  update(){
+
+  }
+
+  delete(queryConfig:any,id:any){
+    const query = gql`
+    mutation  deleteData($queryInput:QueryInput){
+        deleteData(queryInput:$queryInput){
+         data
+        }
+    } `;
+    const reqConfig = {
+      datatable:  queryConfig?.variables?.datatable,
+      id
+    };
+    const variables: any = {
+      queryInput: { ...reqConfig }
+    }
+    return this.apollo.mutate({ mutation: query, variables })
+  }
+
+
+}
--- src/app/pages/page/page.component.ts
+++ src/app/pages/page/page.component.ts
@@ -6,6 +6,7 @@ import { Subscription } from 'rxjs';
 import { Columns } from 'src/app/components/grid/column';
 import { UserConfigService } from 'src/app/user-config.service';
 import { Apollo, gql, QueryRef } from 'apollo-angular';
+import { MasterService } from 'src/app/master.service';
 
 @Component({
   selector: 'app-page',
@@ -16,122 +17,92 @@ export class PageComponent implements OnInit {
 
   breadcrumbList = [];
   gridButtons = []
-  columns:Columns[] = [];
-  rowData:any = {};
-  frameworkComponents:any;
+  columns: Columns[] = [];
+  rowData: any = {};
+  frameworkComponents: any;
   i18n: any;
-  pageConfig:any;
+  pageConfig: any;
   userConfigSub$!: Subscription;
-  pageName:string='';
-  queryConfig:any = {};
-  gridApi:any = {};
-  gridOptions:any = {};
+  pageName: string = '';
+  queryConfig: any = {};
+  gridApi: any = {};
+  gridOptions: any = {};
   private query: QueryRef<any> | undefined;
   constructor(
-    private userService: UserConfigService, 
-    private router:Router,
-    private http:HttpClient,
+    private userService: UserConfigService,
+    private router: Router,
+    private http: HttpClient,
     private route: ActivatedRoute,
     private spinner: NgxSpinnerService,
-    private apollo: Apollo
-    ) { }
+    private apollo: Apollo,
+    private masterService: MasterService
+  ) { }
 
   ngOnInit(): void {
     this.spinner.show();
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.i18n = null;
       this.pageName = String(params.get('name'));
-      if(!this.pageName) return;
+      if (!this.pageName) return;
       this.userConfigSub$ = this.userService.userCast.subscribe((userDetails: any) => {
         if (userDetails.appName) {
-         setTimeout(() => {
-          if( this.userConfigSub$)
-          this.userConfigSub$.unsubscribe();
-          this.i18n = userDetails.i18n[0].translations;
-          this.pageConfig = userDetails[this.pageName];
-          this.spinner.hide();
-         }, 0);
+          setTimeout(() => {
+            if (this.userConfigSub$)
+              this.userConfigSub$.unsubscribe();
+            this.i18n = userDetails.i18n[0].translations;
+            this.pageConfig = userDetails[this.pageName];
+            this.spinner.hide();
+          }, 0);
         }
       });
-    })   
+    })
   }
 
-  fetchData(event:any,queryConfig: any,columns:any=[], title: string) {
+  fetchData(event: any, queryConfig: any, columns: any = [], title: string) {
     this.gridApi[title] = event.params.api;
     this.gridOptions[title] = event.params.api;
     this.queryConfig[title] = queryConfig;
-    const requireColumns:any = []
-    columns.forEach((item:any)=>{
-      if(item.type!== 'action'){
+    const requireColumns: any = []
+    columns.forEach((item: any) => {
+      if (item.type !== 'action') {
         requireColumns.push(item.field)
       }
     });
-    const query = gql`
-      query  ${queryConfig.queryName}($queryInput:QueryInput){
-        ${queryConfig.queryName}(queryInput:$queryInput){
-          data
-        }
-    }
-    `;
-    const reqConfig = queryConfig.variables;
-    reqConfig.columns = requireColumns;
-
-    const variables:any = {
-      queryInput:{...reqConfig}
-    }
-    this.query = this.apollo.watchQuery({
-      query,
-      variables
-    });
-    this.query.valueChanges.subscribe(result => {
-      if(result.data && result.data[queryConfig.queryName]){
+    this.masterService.get(queryConfig, requireColumns).subscribe(result => {
+      if (result.data && result.data[queryConfig.queryName]) {
         const data = JSON.parse(result.data[queryConfig.queryName].data);
         this.rowData[title] = []
-        for(let item in data){
-          data[item].forEach((row:any,index:any) => {            
-            if(!this.rowData[title][index]){
+        for (let item in data) {
+          data[item].forEach((row: any, index: any) => {
+            if (!this.rowData[title][index]) {
               this.rowData[title][index] = {};
             }
             this.rowData[title][index][item] = row;
           });
-        }        
+        }
       }
     });
   }
 
-  onGridBtnClickEvent(event:any){
-    if(event.action === "navigate"){
-      this.router.navigateByUrl(`/${event.url}/${this.pageName}/${event.id?event.id:null}`)
-    }else if(event.action === "delete"){
-      this.spinner.show();
-      const queryConfig = this.queryConfig[event.title]
-    const query = gql`
-    mutation  deleteData($queryInput:QueryInput){
-        deleteData(queryInput:$queryInput){
-         data
-        }
+  onGridBtnClickEvent(event: any) {
+    if (event.action === "navigate") {
+      this.router.navigateByUrl(`/${event.url}/${this.pageName}/${event.id ? event.id : null}`)
+    } else if (event.action === "delete") {
+      this.removeItemFromList(event);
     }
-    `;
-    const reqConfig = {
-      datatable:  queryConfig?.variables?.datatable,
-      id:event.id
-    };
-    const variables: any = {
-      queryInput: { ...reqConfig }
-    }
-    this.apollo.mutate({
-      mutation: query,
-      variables
-    }).subscribe(({ data }) => {
-      const rowIndex = this.rowData[event.title].findIndex((item:any)=>item.id===event.id);
-      this.rowData[event.title].splice(rowIndex,1);
+  }
+  removeItemFromList(event: any) {
+    this.spinner.show();
+    const queryConfig = this.queryConfig[event.title]
+    this.masterService.delete(queryConfig, event.id).subscribe(({ data }) => {
+      const rowIndex = this.rowData[event.title].findIndex((item: any) => item.id === event.id);
+      this.rowData[event.title].splice(rowIndex, 1);
       this.gridApi[event.title].setRowData(this.rowData[event.title]);
       this.spinner.hide();
-    },(error) => {
+    }, (error) => {
       this.spinner.hide();
       console.log('there was an error sending the query', error);
     });
-    }
   }
 
 }
